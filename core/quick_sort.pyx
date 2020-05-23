a39ff9a472192188cca3fdf6d6f969b3f8990e7a
angulartist <belabesmohammed@gmail.com>
2020-05-23 20:05:14 +0200

    quick_sort

---
+++ core/quick_sort.pyx
@@ -0,0 +1,19 @@
+cpdef run(list collection):
+    cdef:
+        int length = len(collection)
+        int pivot, element
+        list greater, lesser
+
+    if length <= 1:
+        return collection
+    else:
+        pivot = collection.pop()
+        greater, lesser = [], []
+
+        for element in collection:
+            if element > pivot:
+                greater.append(element)
+            else:
+                lesser.append(element)
+
+        return run(lesser) + [pivot] + run(greater)
